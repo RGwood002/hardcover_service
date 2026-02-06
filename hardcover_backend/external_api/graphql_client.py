8bad55a454ff5a74eccc4a8f76d7e2e386b9b6d7
Ryan Greenwood <ryan.gwood282@gmail.com>
2026-02-06 15:42:50 -0800

    removed sensative data

--- hardcover_backend/external_api/graphql_client.py
+++ hardcover_backend/external_api/graphql_client.py
@@ -2,9 +2,10 @@ from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 from ..hardcover_exceptions import FailedToGetBookInfo, FailedToInsertUserBook
 import logging
+from decouple import config
 
 logger = logging.getLogger(__name__)
-bearer_token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJIYXJkY292ZXIiLCJ2ZXJzaW9uIjoiOCIsImp0aSI6IjA0NjRmYzJiLTY4ODEtNDZlYS1iYWQ3LTQ0MThiN2IzMDJjNCIsImFwcGxpY2F0aW9uSWQiOjIsInN1YiI6IjY2NDM1IiwiYXVkIjoiMSIsImlkIjoiNjY0MzUiLCJsb2dnZWRJbiI6dHJ1ZSwiaWF0IjoxNzY5MjkxMTAwLCJleHAiOjE4MDA4MjcxMDAsImh0dHBzOi8vaGFzdXJhLmlvL2p3dC9jbGFpbXMiOnsieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJ1c2VyIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1yb2xlIjoidXNlciIsIlgtaGFzdXJhLXVzZXItaWQiOiI2NjQzNSJ9LCJ1c2VyIjp7ImlkIjo2NjQzNX19.fqIP2gn-SAqScWBAzAD8AvLMt6ayVhI8hnArr9Xf-nA"
+bearer_token = config('BEARER_TOKEN')
 
 transport = RequestsHTTPTransport(
     url="https://api.hardcover.app/v1/graphql",
